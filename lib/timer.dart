8b4139a78b82ea3faff58514e13eaf09655f34c5
ddeamaral <deamaral.dylan@gmail.com>
2019-06-11 17:31:20 -0400

    Finishes the timer screen. Had a bug where the progress indicator was NOT between 0 and 1

--- lib/timer.dart
+++ lib/timer.dart
@@ -12,11 +12,11 @@ class Timer extends StatefulWidget {
 
 class _TimerState extends State<Timer> {
   int _secondsValue = 0;
-  double progressValue = 0;
+  int _secondsRemaining = 0;
+  double progressValue = 0.5;
   Stopwatch watch = new Stopwatch();
-  AnimationController animationController;
-  Animation<Color> animation;
   flutter.Timer timer;
+  final double MAX_SLIDER_VALUE = 20;
 
   @override
   void initState() {
@@ -37,16 +37,27 @@ class _TimerState extends State<Timer> {
           children: <Widget>[
             Text(
               "Duration:",
+              style: TextStyle(fontSize: 48),
+            ),
+            Text(
+              "${_secondsRemaining.toString()} seconds",
               style: TextStyle(fontSize: 24),
             ),
-            Text("${_secondsValue.toString()} seconds"),
             Slider(
               value: _secondsValue.toDouble(),
               min: 0,
-              max: 20,
+              max: MAX_SLIDER_VALUE,
               onChanged: (double sliderValue) {
+                watch.reset();
+                if (timer != null) {
+                  timer.cancel();
+                }
+
                 setState(() {
+                  timer = null;
                   _secondsValue = sliderValue.floor();
+                  _secondsRemaining = sliderValue.floor();
+                  progressValue = 0;
                 });
               },
               onChangeEnd: (double sliderValue) {
@@ -56,27 +67,27 @@ class _TimerState extends State<Timer> {
                   return;
                 }
 
-                timer = flutter.Timer.periodic(Duration(seconds: 1), (_timer) {
-                  if (watch.elapsed.inSeconds > 0 && _secondsValue > 0) {
-                    print("calling state overwrite");
-                    print(_secondsValue);
-                    setState(() {
-                      // THIS SET STATE DOESN'T WORK
-                      progressValue =
-                          watch.elapsed.inSeconds / _secondsValue == 0
-                              ? 1
-                              : _secondsValue;
-                      _secondsValue = _secondsValue - watch.elapsed.inSeconds;
-                    });
+                watch.start();
+                timer = flutter.Timer.periodic(Duration(seconds: 1), (t) {
+                  print(
+                      'tick ${t.tick}, ${watch.elapsed.inSeconds.toDouble()} / $_secondsValue = $progressValue, $_secondsValue');
+
+                  setState(() {
+                    progressValue =
+                        watch.elapsed.inSeconds.toDouble() / _secondsValue;
+                    _secondsRemaining = _secondsRemaining - 1;
+                  });
+
+                  if (watch.elapsed.inSeconds >= _secondsValue) {
+                    t.cancel();
+                    watch.reset();
                   }
                 });
-                watch.start();
               },
             ),
             LinearProgressIndicator(
               backgroundColor: Colors.red,
               value: progressValue,
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.pink),
             )
           ],
         ),
