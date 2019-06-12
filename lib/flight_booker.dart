f1712e465500844f4a4afb004e4cc0e125a5c8ce
ddeamaral <deamaral.dylan@gmail.com>
2019-06-11 20:05:13 -0400

    Finishes the flight booker

--- lib/flight_booker.dart
+++ lib/flight_booker.dart
@@ -15,18 +15,17 @@ class FlightBooker extends StatefulWidget {
 }
 
 class _FlightBookerState extends State<FlightBooker> {
-  int selected = 1;
-  bool returnDateValid = true;
+  int selected = 2;
+  bool formValid = true;
   TextEditingController flightDateController;
   TextEditingController returnTextController;
+  String t1 = DateFormat('MM/dd/yyyy').format(DateTime.now());
+  String t2 = DateFormat('MM/dd/yyyy').format(DateTime.now());
+  final _key = GlobalKey<FormState>();
 
   @override
   void initState() {
     super.initState();
-    flightDateController = new TextEditingController(
-        text: DateFormat('MM/dd/yyyy').format(DateTime.now()));
-    returnTextController = new TextEditingController(
-        text: DateFormat('MM/dd/yyyy').format(DateTime.now()));
   }
 
   @override
@@ -36,91 +35,119 @@ class _FlightBookerState extends State<FlightBooker> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              DropdownButton(
-                value: selected,
-                items: <DropdownMenuItem>[
-                  DropdownMenuItem(
-                    child: Center(child: Text("Return Flight")),
-                    value: 1,
-                  ),
-                  DropdownMenuItem(
-                      child: Center(
-                        child: Text("One Way"),
+        body: Builder(
+          builder: (context) => Form(
+                key: _key,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    DropdownButton(
+                      value: selected,
+                      items: <DropdownMenuItem>[
+                        DropdownMenuItem(
+                          child: Center(child: Text("Return Flight")),
+                          value: 1,
+                        ),
+                        DropdownMenuItem(
+                            child: Center(
+                              child: Text("One-Way flight"),
+                            ),
+                            value: 2)
+                      ],
+                      isExpanded: true,
+                      onChanged: (item) {
+                        setState(() {
+                          selected = item;
+                        });
+                      },
+                    ),
+                    Center(
+                      child: Text(
+                        "T1",
+                        style: TextStyle(fontSize: 20),
                       ),
-                      value: 2)
-                ],
-                isExpanded: true,
-                onChanged: (item) {
-                  setState(() {
-                    selected = item;
-                  });
-                },
-              ),
-              Center(
-                child: Text(
-                  "T1",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              TextField(
-                controller: flightDateController,
-                decoration: InputDecoration(
-                    filled: !dateIsValid(flightDateController.text),
-                    fillColor: Colors.red),
-                keyboardType: TextInputType.datetime,
-              ),
-              Center(
-                child: Text(
-                  "T2",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              TextField(
-                controller: returnTextController,
-                decoration: InputDecoration(
-                    filled: !dateIsValid(returnTextController.text),
-                    fillColor: Colors.red),
-                keyboardType: TextInputType.datetime,
-              ),
-              RaisedButton(
-                onPressed: dateIsValid(returnTextController.text) &&
-                        dateIsValid(flightDateController.text)
-                    ? () {
-                        if (selected == 2) {
-                          Scaffold.of(context).showSnackBar(new SnackBar(
+                    ),
+                    TextFormField(
+                      initialValue:
+                          DateFormat('MM/dd/yyyy').format(DateTime.now()),
+                      validator: (value) {
+                        if (!dateIsValid(value)) {
+                          return "Please enter a valid date";
+                        }
+
+                        return null;
+                      },
+                      onSaved: (value) => setState(() {
+                            t1 = value;
+                          }),
+                      decoration: InputDecoration(
+                          filled: !dateIsValid(t1), fillColor: Colors.red),
+                      keyboardType: TextInputType.datetime,
+                    ),
+                    Center(
+                      child: Text(
+                        "T2",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    TextFormField(
+                      initialValue:
+                          DateFormat('MM/dd/yyyy').format(DateTime.now()),
+                      validator: (value) {
+                        if (!dateIsValid(value)) {
+                          return "Please enter a valid date";
+                        }
+
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        filled: !dateIsValid(t2),
+                        fillColor: Colors.red,
+                      ),
+                      onSaved: (value) => setState(() {
+                            t2 = value;
+                          }),
+                      keyboardType: TextInputType.datetime,
+                      enabled: selected == 1,
+                    ),
+                    RaisedButton(
+                      onPressed: () {
+                        if (selected == 2 && _key.currentState.validate()) {
+                          Scaffold.of(context).showSnackBar(SnackBar(
                             content: new Text(
-                                "You have booked a one-way flight on ${DateFormat("MM.dd.yyyy").format(DateTime.parse(flightDateController.text))}"),
+                                "You have booked a one-way flight on ${DateFormat("MM.dd.yyyy").format(tryParse(t1))}"),
                           ));
                         }
-                      }
-                    : null,
-                child: Text(
-                  "Book",
-                  style: TextStyle(fontSize: 20),
+                      },
+                      child: Text(
+                        "Book",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
         ),
       ),
     );
   }
 
   bool dateIsValid(String value) {
-    if (value.isEmpty) {
+    DateTime t = tryParse(value);
+
+    if (t != null && t.isAfter(DateTime.now().subtract(Duration(days: 1)))) {
       return true;
     }
 
+    return false;
+  }
+
+  DateTime tryParse(String value) {
     DateTime t;
     try {
       t = DateFormat("MM/dd/yyyy").parse(value);
-      print(t);
     } catch (e) {}
 
     try {
@@ -131,11 +158,23 @@ class _FlightBookerState extends State<FlightBooker> {
       t = DateFormat("MM-dd-yyyy").parse(value);
     } catch (e) {}
 
-    if (t != null &&
-        t.isAfter(DateTime.now().subtract(new Duration(days: 365 * 20)))) {
-      return true;
-    }
+    return t;
+  }
 
-    return false;
+  void isFormValid() {
+    DateTime t = dateIsValid(flightDateController.text)
+        ? tryParse(flightDateController.text)
+        : null;
+    DateTime t2 = dateIsValid(returnTextController.text)
+        ? tryParse(flightDateController.text)
+        : null;
+
+    bool returnFlightValid =
+        !(selected == 1 && t != null && t2 != null && t2.isBefore(t));
+    bool oneWayFlightValid = selected == 2 && t != null;
+
+    setState(() {
+      formValid = returnFlightValid || oneWayFlightValid;
+    });
   }
 }
